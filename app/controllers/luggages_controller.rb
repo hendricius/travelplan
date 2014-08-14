95668a85767ef851566d586c70b3f5906f07233b
Prelang Builder <builder@prelang.com>
2014-08-14 19:05:13 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/luggages_controller.rb
+++ app/controllers/luggages_controller.rb
@@ -1,5 +1,5 @@
 class LuggagesController < ApplicationController
-  before_action :set_luggage, only: [:show, :edit, :update, :destroy]
+  before_action :set_luggage, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /luggages
   # GET /luggages.json
