f962143ae2fa1bf7d2f6a0949dd82591e88012b0
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2014-08-14 21:54:02 +0200

    rework whole ui

--- app/models/luggage.rb
+++ app/models/luggage.rb
@@ -1,5 +1,6 @@
 class Luggage < ActiveRecord::Base
   belongs_to :trip
   belongs_to :participant
-  acts_as_votable 
+  belongs_to :responsible_person, class_name: "Participant", foreign_key: :taken_care_of_by_id
+  acts_as_votable
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,3 +1,5 @@
 class Participant < ActiveRecord::Base
   belongs_to :trip
+  has_many :luggages
+  has_many :responsible_luggages
 end
